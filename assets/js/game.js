8253338eec53b2ecfa8134cfb58a03c0a9df1b27
Aaron Bean <arbean1978@gmail.com>
2021-11-08 16:52:41 -0700

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,40 @@ var randomNumber = function(min,max) {
 
   return value;
 };
+
+var fightOrSkip = function() {
+  //ask player if they'd like to fight or skip using the fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  
+  promptFight = promptFight.toLowerCase();
+
+  //Conditional Recursive Function Call
+  if(promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+  }
+
+  //If player picks 'skip' confirm and then stop the loop
+  if(promptFight === "skip" || promptFight === "SKIP") {
+    //confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure youd like to quit?");
+
+    //if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playInfo.name + " has decided to skip this fight. Goodbye!");
+      //subtract money from playerMoney for skipping
+      playerInfo.playerMoney = playerInfo.money - 10;
+      shop()
+    }
+  }
+}
 //fight funtion
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    if (fightOrSkip());
+    break;
 
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
       // confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -41,6 +67,7 @@ var fight = function(enemy) {
 
       // leave while() loop since enemy is dead
       break;
+      
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
@@ -60,8 +87,8 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
-  }
-};
+  };
+
 // run fight function to start game
 var startGame = function() {
   //reset player stats
